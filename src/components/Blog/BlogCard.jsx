905b46e2327df0111489433a400342657101ef86
0800WEB <144950728+lucasidiotlab@users.noreply.github.com>
2024-06-19 12:48:09 -0300

    cambios

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const BlogCard = () => {
     return (
+        <>
         <div className="w-[483.78px] h-[490px] flex-col justify-start items-start gap-4 inline-flex">
             <div className="w-[483.78px] h-[391px] bg-black/opacity-30 rounded-md" />
             <div className="self-stretch text-neutral-800 text-xl font-bold font-['Helvetica Neue LT Std']">De esta manera logramos publicidad efectiva en nuestros clientes</div>
@@ -10,6 +11,7 @@ const BlogCard = () => {
                 <div className="w-[19px] h-[19px] relative" />
             </div>
         </div>
+        </>
     )
 }
 
